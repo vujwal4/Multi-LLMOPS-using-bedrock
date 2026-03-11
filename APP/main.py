8cf6a7755545b37bfd51f7c934138ba0dfcdde04
vujwal4 <vujwalm@gmail.com>
2026-03-11 15:34:58 +0530

    commit

--- APP/main.py
+++ APP/main.py
@@ -20,7 +20,7 @@ def run_backend():
 def run_frontend():
     try:
         logger.info("Starting Frontend service")
-        subprocess.Popen(["streamlit","run","APP/frontend/ui.py","--server.address","0.0.0.0","--server.port","8501"],check = True)
+        subprocess.run(["streamlit" , "run" , "APP/frontend/ui.py"],check=True)
 
     except CustomException as e:
         logger.error("Problem with frontend service")
